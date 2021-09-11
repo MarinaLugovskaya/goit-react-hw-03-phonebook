7688b3ef5a68150ff35bc314eae1139280eadf23
Marina Lugovskaya <anomaliya1993@gmail.com>
2021-09-11 14:20:24 +0300

    new2

---
+++ src/components/App.jsx
@@ -0,0 +1,84 @@
+import React, { Component } from "react";
+import Form from "../components/Form/Form";
+import Contacts from "../components/Contacts/Contacts";
+import Filter from "../components/Filter/Filter";
+import { v4 as uuidv4 } from "uuid";
+import css from "../components/Form/Form.module.css";
+
+export default class App extends Component {
+  state = {
+    contacts: [],
+    filter: "",
+  };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem("contacts");
+    const parsedContacts = JSON.parse(contacts);
+    this.setState({ contacts: parsedContacts });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log(prevState.contacts);
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+  }
+
+  getVisibleContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  handleDeleteContacts = (evt) => {
+    this.setState({
+      contacts: this.state.contacts.filter(
+        (contact) => contact.id !== evt.target.id
+      ),
+    });
+  };
+
+  formSubmitHandler = (name, number) => {
+    const { contacts } = this.state;
+    if (
+      contacts.some(
+        (contact) => contact.name.toLowerCase() === name.toLowerCase()
+      )
+    ) {
+      alert(`${name} is already exists in contact list`);
+      return;
+    }
+    this.state.contacts.push({ name: name, id: uuidv4(), number: number });
+    this.setState({ contacts: contacts });
+  };
+
+  changeFilter = (evt) => {
+    this.setState({ filter: evt.currentTarget.value });
+  };
+
+  render() {
+    const visibleContacts = this.getVisibleContacts();
+    return (
+      <>
+        <div className={css.postcard}>
+          <h1>Phonebook</h1>
+          <Form contacts={this.contacts} onSubmit={this.formSubmitHandler} />
+          <div className={css.formRow}>
+            <Filter value={this.filter} onChange={this.changeFilter} />
+          </div>
+          <h2>Contacts</h2>
+          <Contacts
+            filter={this.filter}
+            onChange={this.HandleSearchContactByName}
+            onClick={this.handleDeleteContacts}
+            // contacts={contacts}
+            contacts={visibleContacts}
+          />
+        </div>
+      </>
+    );
+  }
+}
---
+++ src/components/Contacts/Contacts.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+
+const Contacts = ({ contacts, onClick }) => {
+  return (
+    <ul>
+      {contacts.map(contact => (
+        <li key={contact.id}>
+          <p>
+            {contact.name}: {contact.number}
+          </p>
+          <button id={contact.id} onClick={onClick} type="button">
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Contacts;
+
+Contacts.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    }).isRequired,
+  ).isRequired,
+};
